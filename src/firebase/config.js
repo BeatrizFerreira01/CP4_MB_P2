3760130b40c41edf1d23991d25375e877b223afa
Beatriz Ferreira Cruz <161889623+BeatrizFerreira01@users.noreply.github.com>
2025-09-16 21:11:12 -0300

    Clear API keys in firebase config

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDm32Y5FPyeMdp6jElL9I8IwNddxRZEGgI",
+  apiKey: "",
   authDomain: "lista-tarefas-plus-561a4.firebaseapp.com",
   projectId: "lista-tarefas-plus-561a4",
   storageBucket: "lista-tarefas-plus-561a4.appspot.com",
-  messagingSenderId: "920910901009",
-  appId: "1:920910901009:web:d02d3c1802b5cd764366b0"
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Firebase
